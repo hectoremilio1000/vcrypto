796ceace6a8b78123939e8e7a22bc261c1a568d1
hectoremilio1000 <hectoremilio1000@gmail.com>
2021-07-30 13:14:54 -0500

    HomeScreenUI

---
+++ src/screens/HomeScreen/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  root: {
+    padding: 20,
+    alignItems: "center",
+    flex: 1,
+    backgroundColor: "white",
+  },
+  image: {
+    height: "40%",
+    aspectRatio: 1,
+  },
+  header1: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+  header2: {
+    fontSize: 20,
+    color: "#707070",
+    textAlign: "center",
+  },
+});
+
+export default styles;
